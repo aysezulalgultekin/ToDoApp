5d50ebdba92a25bc083615a2d491c48e4d59628e
aysezulalgultekin <aysezulalgultekin@hotmail.com>
2025-01-16 22:41:43 +0300

    RabbitFragment Drawable Code Added

--- app/src/main/java/com/example/todoappdemo/RabbitFragment.java
+++ app/src/main/java/com/example/todoappdemo/RabbitFragment.java
@@ -1,23 +1,20 @@
 package com.example.todoappdemo;
 
 import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.Objects;
 
 
@@ -34,7 +31,8 @@ public class RabbitFragment extends Fragment {
     private void updateRabbitImage(View view) {
         String userId = Objects.requireNonNull(SessionManager.getCurrentUser()).getUserId();
 
-        userRef.child("level").addValueEventListener(new ValueEventListener() {
+        userRef.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 int totalTasks = 0;
@@ -50,32 +48,116 @@ public class RabbitFragment extends Fragment {
                         }
                     }
                 }
+
                 if (totalTasks > 0) {
+                    Integer levelInteger = snapshot.child(userId).child("level").getValue(Integer.class);
+                    int level = Objects.requireNonNullElse(levelInteger, 0);
                     double completionRate = (double) completedTasks / totalTasks * 100;
-                    Integer levelInteger = snapshot.child("level").getValue(Integer.class);
-                    if (levelInteger != null) {
-                        int level = levelInteger;
-                        getRabbitImageByLevel(completionRate, view, level);
-                    }
+                    updateRabbitImageDrawable(completionRate, level, view);
                 }
+
                 Log.d("Firebase", "Total tasks: " + totalTasks + ", Completed tasks: " + completedTasks);
             }
 
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("Firebase", "Failed to fetch user level: " + error.getMessage());
+                Log.e("Firebase", "Failed to fetch tasks: " + error.getMessage());
             }
         });
     }
-
     @SuppressLint("UseCompatLoadingForDrawables")
-    private void getRabbitImageByLevel(double completionRate, View view, int level) {
+    private void updateRabbitImageDrawable(double completionRate, int level, View view) {
         ImageView ivRabbit = view.findViewById(R.id.iv_bunny);
-        //lvl 0: 0, lvl 1: 20 - flowers, lvl 2: 50 - objects, lvl 3 - chick: 70-, lvl 4 - bird: 90
-        //level
+        String mood;
 
+        if (completionRate >= 60) {
+            mood = "happy";
+        } else if (completionRate >= 40) {
+            mood = "normal";
+        } else {
+            mood = "sad";
+        }
 
+        int rank;
+        if (level >= 90) {
+            rank = 4;
+        } else if (level >= 70) {
+            rank = 3;
+        } else if (level >= 50) {
+            rank = 2;
+        } else if (level >= 20) {
+            rank = 1;
+        } else {
+            rank = 0;
+        }
+
+        Drawable drawable = null;
+        switch (mood) {
+            case "happy":
+                switch (rank) {
+                    case 4:
+                        drawable = getResources().getDrawable(R.drawable.happy_rank_4);
+                        break;
+                    case 3:
+                        drawable = getResources().getDrawable(R.drawable.happy_rank_3);
+                        break;
+                    case 2:
+                        drawable = getResources().getDrawable(R.drawable.happy_rank_2);
+                        break;
+                    case 1:
+                        drawable = getResources().getDrawable(R.drawable.happy_rank_1);
+                        break;
+                    default:
+                        drawable = getResources().getDrawable(R.drawable.happy_rank_0);
+                        break;
+                }
+                break;
+            case "normal":
+                switch (rank) {
+                    case 4:
+                        drawable = getResources().getDrawable(R.drawable.normal_rank_4);
+                        break;
+                    case 3:
+                        drawable = getResources().getDrawable(R.drawable.normal_rank_3);
+                        break;
+                    case 2:
+                        drawable = getResources().getDrawable(R.drawable.normal_rank_2);
+                        break;
+                    case 1:
+                        drawable = getResources().getDrawable(R.drawable.normal_rank_1);
+                        break;
+                    default:
+                        drawable = getResources().getDrawable(R.drawable.normal_rank_0);
+                        break;
+                }
+                break;
+            case "sad":
+                switch (rank) {
+                    case 4:
+                        drawable = getResources().getDrawable(R.drawable.sad_rank_4);
+                        break;
+                    case 3:
+                        drawable = getResources().getDrawable(R.drawable.sad_rank_3);
+                        break;
+                    case 2:
+                        drawable = getResources().getDrawable(R.drawable.sad_rank_2);
+                        break;
+                    case 1:
+                        drawable = getResources().getDrawable(R.drawable.sad_rank_1);
+                        break;
+                    default:
+                        drawable = getResources().getDrawable(R.drawable.sad_rank_0);
+                        break;
+                }
+                break;
+        }
+
+        if (drawable != null) {
+            ivRabbit.setImageDrawable(drawable);
+        } else {
+            Log.e("RabbitImage", "Drawable not found for mood: " + mood + ", rank: " + rank);
+        }
     }
 
 }
+
